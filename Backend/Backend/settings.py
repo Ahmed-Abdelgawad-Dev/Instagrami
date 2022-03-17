e609df97e41a492336d34da9ab5d0d71d28f96c5
Ahmed-Abdelgawad-Dev <ahmed.abdelgawad.dev@gmail.com>
2022-03-17 23:27:56 +0200

    Ammended backend authentication&authorization

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     # 'rest_framework.authtoken',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
@@ -80,7 +81,9 @@ DATABASES = {
 
 
 REST_FRAMEWORK = {
-
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
 
         'rest_framework_simplejwt.authentication.JWTAuthentication',
@@ -88,7 +91,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
--- Backend/users/urls.py
+++ Backend/users/urls.py
@@ -1,12 +1,7 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import UserViewSet
+from django.urls import path
+from .views import RegisterView, LoginView
 
-# Create a router and register our viewsets with it.
-router = DefaultRouter()
-router.register(r'users', UserViewSet)
-
-# The API URLs are now determined automatically by the router.
 urlpatterns = [
-    path('', include(router.urls)),
+    path('register', RegisterView.as_view()),
+    path('user', LoginView.as_view()),
 ]
