8ef8266f1c8a8207f3cd90f1225a857c676885f5
Ahmed-Abdelgawad-Dev <ahmed.abdelgawad.dev@gmail.com>
2022-03-20 03:58:23 +0200

    Refactoring to fit Pycharm IDE

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -177,8 +177,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
+    'http://localhost:3001',
     'http://localhost:3002',
-    'http://localhost:3003',
 ]
 CORS_ALLOW_CREDENTIALS = True
 
