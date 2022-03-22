2c2ff2e72abe00186f9019f7f670631c07185977
Ahmed-Abdelgawad-Dev <ahmed.abdelgawad.dev@gmail.com>
2022-03-22 23:12:56 +0200

    Refactoring

--- Frontend/pages/post/[slug].tsx
+++ Frontend/pages/post/[slug].tsx
@@ -4,9 +4,9 @@ export default function () {
   return <div></div>;
 }
 
-// export const getStaticPaths = async () => {
-//   const res = await fetch("http://127.0.0.1:8000/posts");
-//   const data = await res.json();
-//   const parsed= JSON.parse(data)
-//   const paths:Post  = parsed
-// };
+export const getStaticPaths = async () => {
+  const res = await fetch("http://127.0.0.1:8000/posts");
+  const data = await res.json();
+  const parsed= JSON.parse(data)
+  const paths:Post  = parsed
+};
