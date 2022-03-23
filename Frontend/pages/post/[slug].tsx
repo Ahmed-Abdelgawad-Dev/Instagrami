b9a161e745db004cfd04cb6b008dcdfb23678388
Ahmed-Abdelgawad-Dev <ahmed.abdelgawad.dev@gmail.com>
2022-03-23 02:12:25 +0200

    Refactoring

--- Frontend/pages/post/[slug].tsx
+++ Frontend/pages/post/[slug].tsx
@@ -1,16 +1,48 @@
+import Header from "../../components/Header";
 import { Post } from "../../typings/interfaces";
+import { GetStaticProps } from "next";
 
-export default function () {
-  return <div></div>;
+interface Props {
+  post: Post;
 }
+
+const Post = ({ post }: Props) => {
+  console.log(post);
+
+  return (
+    <main>
+      <Header />
+    </main>
+  );
+};
+
+export default Post;
 
 export const getStaticPaths = async () => {
   const res = await fetch("http://127.0.0.1:8000/posts");
-  const posts = await res.json();
+  const res2 = await res.json();
+  const posts = JSON.parse(res2);
 
-  
   const paths = posts.map((post: Post) => ({
-      params: { 
-          slug: post.slug
-      }
-  }))
+    params: {
+      slug: post.slug,
+    },
+  }));
+  return {
+    paths,
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  let slug = `0`;
+  const query = `http://127.0.0.1:8000/posts/${slug}`;
+  const post = await fetch(query, {
+    slug: params?.slug,
+  } as any);
+
+  if (!post) {
+    return { notFound: true };
+  }
+  return { props: { post } };
+};
